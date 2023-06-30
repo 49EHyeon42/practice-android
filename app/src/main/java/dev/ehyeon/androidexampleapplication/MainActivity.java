1abdac550b40b935b1126998dd8008b79eb4b40d
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-06-30 19:41:50 +0900

    refactor: user view model factory 분리

--- app/src/main/java/dev/ehyeon/androidexampleapplication/MainActivity.java
+++ app/src/main/java/dev/ehyeon/androidexampleapplication/MainActivity.java
@@ -6,10 +6,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
 import javax.inject.Inject;
@@ -31,17 +29,8 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = findViewById(R.id.listView);
         Button button = findViewById(R.id.button);
 
-        userViewModel = new ViewModelProvider(this, new ViewModelProvider.Factory() {
-            @NonNull
-            @Override
-            @SuppressWarnings("unchecked")
-            public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
-                if (modelClass.isAssignableFrom(UserViewModel.class)) {
-                    return (T) new UserViewModel(userRepository);
-                }
-                throw new IllegalArgumentException();
-            }
-        }).get(UserViewModel.class);
+        userViewModel = new ViewModelProvider(this, new UserViewModelFactory(userRepository))
+                .get(UserViewModel.class);
 
         CustomAdapter adapter = new CustomAdapter(this, userViewModel.findAllUser());
 
