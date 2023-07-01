566721714f82e223bb790caa5ac063a47353ebcc
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-07-01 15:22:18 +0900

    feat: View Binding 적용

--- app/src/main/java/dev/ehyeon/androidexampleapplication/MainActivity.java
+++ app/src/main/java/dev/ehyeon/androidexampleapplication/MainActivity.java
@@ -2,9 +2,7 @@ package dev.ehyeon.androidexampleapplication;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +10,11 @@ import androidx.lifecycle.ViewModelProvider;
 
 import javax.inject.Inject;
 
+import dev.ehyeon.androidexampleapplication.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Inject
     UserViewModelFactory userViewModelFactory;
@@ -20,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         EHyeonComponent component = ((EHyeonApplication) getApplication()).getComponent();
         component.inject(this);
@@ -28,19 +31,16 @@ public class MainActivity extends AppCompatActivity {
         UserViewModel userViewModel = new ViewModelProvider(this, userViewModelFactory)
                 .get(UserViewModel.class);
 
-        ListView listView = findViewById(R.id.listView);
-        Button button = findViewById(R.id.button);
-
         CustomAdapter adapter = new CustomAdapter(this, userViewModel.findAllUser());
 
-        listView.setAdapter(adapter);
+        binding.listView.setAdapter(adapter);
 
         userViewModel.findAllUserToLiveData().observe(this, users -> {
             adapter.updateList(users);
             adapter.notifyDataSetChanged();
         });
 
-        button.setOnClickListener(view -> {
+        binding.button.setOnClickListener(view -> {
             AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
 
             View dialogView = View.inflate(this, R.layout.activity_main_customdialog, null);
