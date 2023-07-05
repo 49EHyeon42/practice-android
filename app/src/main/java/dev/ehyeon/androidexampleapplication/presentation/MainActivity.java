98f348e61b0940211773676e242539f77c57f2b4
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-07-05 15:26:43 +0900

    fix: bottom navigation 작업 전에 추가한 recycler view, room 등 을 dagger fragment로 이전

--- app/src/main/java/dev/ehyeon/androidexampleapplication/presentation/MainActivity.java
+++ app/src/main/java/dev/ehyeon/androidexampleapplication/presentation/MainActivity.java
@@ -7,8 +7,6 @@ import androidx.fragment.app.Fragment;
 
 import java.util.stream.Stream;
 
-import javax.inject.Inject;
-
 import dev.ehyeon.androidexampleapplication.EHyeonApplication;
 import dev.ehyeon.androidexampleapplication.R;
 import dev.ehyeon.androidexampleapplication.databinding.ActivityMainBinding;
@@ -16,49 +14,12 @@ import dev.ehyeon.androidexampleapplication.di.EHyeonComponent;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
-
-    @Inject
-    UserViewModelFactory userViewModelFactory;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        EHyeonComponent component = ((EHyeonApplication) getApplication()).getComponent();
-        component.inject(this);
-
-//        UserViewModel userViewModel = new ViewModelProvider(this, userViewModelFactory)
-//                .get(UserViewModel.class);
-//
-//        CustomAdapter adapter = new CustomAdapter(userViewModel.findAllUser());
-//
-//        binding.recyclerView.setAdapter(adapter);
-//        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//        binding.recyclerView.addItemDecoration(new DividerItemDecoration(this, 1)); // 구분선
-//
-//        userViewModel.findAllUserToLiveData().observe(this, adapter::updateList);
-//
-//        binding.button.setOnClickListener(view -> {
-//            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-//
-//            View dialogView = View.inflate(this, R.layout.activity_main_customdialog, null);
-//
-//            dialogBuilder.setView(dialogView);
-//
-//            dialogBuilder.setPositiveButton("확인", (dialog, which) -> {
-//                EditText etEmail = dialogView.findViewById(R.id.etEmail);
-//                EditText etName = dialogView.findViewById(R.id.etName);
-//
-//                userViewModel.saveUser(new UserDto(etEmail.getText().toString(), etName.getText().toString()));
-//            });
-//
-//            dialogBuilder.setNegativeButton("취소", null);
-//
-//            dialogBuilder.show();
-//        });
         initMainFragment();
 
         binding.bottomNavigation.setOnItemSelectedListener(item -> updateFragmentById(item.getItemId()));
@@ -104,5 +65,9 @@ public class MainActivity extends AppCompatActivity {
 
     private Fragment getFragmentById(int id) {
         return getSupportFragmentManager().findFragmentByTag(MainFragmentType.of(id).getTag());
+    }
+
+    public EHyeonComponent getComponent() {
+        return ((EHyeonApplication) getApplication()).getComponent();
     }
 }
